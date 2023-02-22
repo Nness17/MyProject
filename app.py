52dcc378a8a7a619c0c26e00c0cd5e24a54b209a
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-02-22 10:40:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,75 @@
 import streamlit as st
-import random
 
-# Genera un numero casuale compreso tra 1 e 100
-numero_da_indovinare = random.randint(1, 100)
+# Creazione del tavolo da gioco
+board = [[None for j in range(4)] for i in range(4)]
 
-# Inizializza il contatore dei tentativi a 0
-tentativi = 0
+# Funzione che controlla se c'è un quarto
+def check_win():
+    for i in range(4):
+        # Controllo delle righe
+        if all(board[i][j] is not None for j in range(4)) and len(set(board[i])) == 1:
+            return True
+        # Controllo delle colonne
+        if all(board[j][i] is not None for j in range(4)) and len(set([board[j][i] for j in range(4)])) == 1:
+            return True
+    # Controllo delle diagonali
+    if all(board[i][i] is not None for i in range(4)) and len(set([board[i][i] for i in range(4)])) == 1:
+        return True
+    if all(board[i][3-i] is not None for i in range(4)) and len(set([board[i][3-i] for i in range(4)])) == 1:
+        return True
+    return False
 
-st.write("# Indovina il numero")
+# Funzione che controlla se la partita è finita
+def check_game_over():
+    return all(all(row) for row in board) or check_win()
+
+# Creazione dei bottoni per selezionare la pedina da posizionare
+available_pieces = [i+1 for i in range(16)]
+selected_piece = None
+
+# Creazione dell'interfaccia grafica
+st.title("Quarto")
+
+# Creazione del tavolo da gioco
+board_elements = []
+for i in range(4):
+    for j in range(4):
+        board_elements.append(st.empty())
 
 # Ciclo di gioco
-while True:
-    tentativo = st.number_input("Inserisci un numero tra 1 e 100", min_value=1, max_value=100)
-    tentativi += 1
-    if tentativo < numero_da_indovinare:
-        st.write("Il tuo tentativo è troppo basso.")
-    elif tentativo > numero_da_indovinare:
-        st.write("Il tuo tentativo è troppo alto.")
-    else:
-        st.write("Hai indovinato il numero in {} tentativi!".format(tentativi))
+while not check_game_over():
+
+    # Stampa del tavolo da gioco
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] is None:
+                board_elements[i*4+j].markdown("-")
+            else:
+                board_elements[i*4+j].markdown(f"{board[i][j]}")
+
+    # Selezione della pedina da posizionare
+    st.subheader("Seleziona la pedina da posizionare")
+    selected_piece = st.radio("", available_pieces) - 1
+    available_pieces.remove(selected_piece + 1)
+
+    # Selezione della posizione dove posizionare la pedina
+    st.subheader("Seleziona la posizione in cui posizionare la pedina")
+    row = st.selectbox("Riga", [0, 1, 2, 3])
+    col = st.selectbox("Colonna", [0, 1, 2, 3])
+
+    # Posizionamento della pedina
+    board[row][col] = selected_piece
+
+    # Controllo della vittoria
+    if check_win():
+        st.success("Hai vinto!")
         break
 
-
-
+# Stampa del tavolo finale
+st.subheader("Tavolo finale")
+for i in range(4):
+    for j in range(4):
+        st.markdown(f"{board[i][j]}")
 
 
 
