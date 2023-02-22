548c924926e293b31f871ce23d5de1a6764845ff
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-02-22 10:57:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,13 @@
-import streamlit as st
-
-questions = [
-    "Sei felice quando sei con i tuoi amici?",
-    "Hai paura dei cani?",
-    "Ti piace leggere libri?",
-    "Sei bravo/a a fare amicizia?",
-    "Ti piace fare sport?",
-    "Ti piace la musica?",
-    "Hai mai viaggiato in un altro paese?",
-    "Ti piace la scuola?",
-    "Hai un animale domestico?",
-    "Hai un sogno nel cassetto?",
-]
-
-def show_questions():
-    answers = []
-    
-    for question in questions:
-        answer = st.radio(question, ("Sì", "No"))
-        answers.append(answer)
-    
-    return answers
-
-def calculate_result(answers):
-    score = 0
-    
-    for answer in answers:
-        if answer == "Sì":
-            score += 1
-    
-    if score <= 3:
-        return "Sei una persona timida, ma gentile e dolce."
-    elif score <= 7:
-        return "Sei una persona estroversa e socievole."
-    else:
-        return "Sei una persona ambiziosa e determinata."
-
 def main():
+    st.sidebar.title("Seleziona una pagina")
+    page = st.sidebar.radio("", ("Domande", "Risultato"))
+
+    if page == "Domande":
+        show_questions_page()
+    else:
+        show_result_page()
+        
+def show_questions_page():
     st.title("Test della Personalità per Bambini")
     st.write("Rispondi alle seguenti domande per scoprire qual è la tua personalità!")
     
@@ -44,21 +15,17 @@ def main():
     
     if st.button("Mostra il risultato"):
         result = calculate_result(answers)
-        st.write("Il tuo risultato è:", result)
+        st.session_state["result"] = result
+        st.experimental_rerun()
+
+def show_result_page():
+    st.title("Risultato del Test della Personalità per Bambini")
     
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-
+    if "result" in st.session_state:
+        result = st.session_state["result"]
+        st.write("Il tuo risultato è:", result)
+    else:
+        st.write("Prima devi completare il test!")
 
 
 
