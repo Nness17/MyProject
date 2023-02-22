78d3f28a226276bccd52d83f20ee4b5e730510d2
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-02-22 11:03:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,56 @@
-def main():
-    st.sidebar.title("Seleziona una pagina")
-    page = st.sidebar.radio("", ("Domande", "Risultato"))
+import streamlit as st
 
-    if page == "Domande":
-        show_questions_page()
+questions = [
+    "Sei felice quando sei con i tuoi amici?",
+    "Hai paura dei cani?",
+    "Ti piace leggere libri?",
+    "Sei bravo/a a fare amicizia?",
+    "Ti piace fare sport?",
+    "Ti piace la musica?",
+    "Hai mai viaggiato in un altro paese?",
+    "Ti piace la scuola?",
+    "Hai un animale domestico?",
+    "Hai un sogno nel cassetto?",
+]
+
+def show_question(index):
+    question = questions[index]
+    answer = st.radio(question, ("Sì", "No"))
+    return answer
+
+def calculate_result(answers):
+    score = 0
+    
+    for answer in answers:
+        if answer == "Sì":
+            score += 1
+    
+    if score <= 3:
+        return "Sei una persona timida, ma gentile e dolce."
+    elif score <= 7:
+        return "Sei una persona estroversa e socievole."
     else:
-        show_result_page()
-        
-def show_questions_page():
+        return "Sei una persona ambiziosa e determinata."
+    
+    def main():
     st.title("Test della Personalità per Bambini")
     st.write("Rispondi alle seguenti domande per scoprire qual è la tua personalità!")
     
-    answers = show_questions()
+    answers = []
+    for i, question in enumerate(questions):
+        st.write("Domanda", i + 1)
+        answer = show_question(i)
+        answers.append(answer)
     
     if st.button("Mostra il risultato"):
         result = calculate_result(answers)
         st.session_state["result"] = result
         st.experimental_rerun()
 
-def show_result_page():
-    st.title("Risultato del Test della Personalità per Bambini")
-    
     if "result" in st.session_state:
+        st.title("Risultato del Test della Personalità per Bambini")
         result = st.session_state["result"]
         st.write("Il tuo risultato è:", result)
-    else:
-        st.write("Prima devi completare il test!")
 
-
-
-
-
+if __name__ == "__main__":
+    main()
