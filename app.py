83e57b31dd01e1d339993e36990ebf0191deec8f
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-03-03 10:23:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,14 @@ def main():
         answers.append(answer)
         st.write(" ")
         
-        
+    result, image_url = calculate_result(answers)
+    if st.button('Submit your answers'):
+            if result:
+                st.write(f"And you aree... {result}!")
+                st.write(" ")
+                st.image(image_url, caption=result, use_column_width=True)
+            else:
+                st.write("Nessun eroe corrisponde alle tue risposte.")        
     
     # calcoliamo il risultato in base alle risposte date
     result, image_url = calculate_result(answers)
@@ -117,12 +124,5 @@ with st.sidebar:
         st.title("Lets start the test!!! :boy: :girl:")
         
 #mostra risultato 
-result, image_url = calculate_result(answers)
-if st.button('Submit your answers'):
-        if result:
-            st.write(f"And you aree... {result}!")
-            st.write(" ")
-            st.image(image_url, caption=result, use_column_width=True)
-        else:
-            st.write("Nessun eroe corrisponde alle tue risposte.")
+
             
