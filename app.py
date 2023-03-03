f46775947faf241b8a8347d4da79815b3b7c1419
Nness17 <124429050+Nness17@users.noreply.github.com>
2023-03-03 13:23:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,42 +16,74 @@ questions = [
     },
     {
         "question": "Where do you want to be?",
-        "options": ["In the magic reign of magma", "In the ice galaxy", "In a small cubic planet", "In an infinity spaceship"]
+        "options": ["In the magic reign of magma", "In the ice galaxy", "In a massive jungle", "In an infinity spaceship"]
     }
 ]
 
 # definiamo una lista di possibili eroi e i loro risultati associati
 heroes = {
     "The super knight of magma": {
-        "criteria": ["A dragon with 3 heads", "A fire sword", "Super strenght", "In the magic reign of magma"],
+        "criteria": ["A fire sword", "In the magic reign of magma"],
         "image": "https://example.com/batman.jpg"
     },
     "The super archer of magma": {
-        "criteria": ["A dragon with 3 heads", "A magic bow that throws 3 arrows", "Super strenght", "In the magic reign of magma"],
+        "criteria": ["A magic bow that throws 3 arrows", "In the magic reign of magma"],
         "image": "https://example.com/superman.jpg"
     },
     "The super paladin of magma ": {
-        "criteria": ["A dragon with 3 heads", "A hammer with lightnings", "Super strenght", "In the magic reign of magma"],
+        "criteria": ["A hammer with lightnings", "In the magic reign of magma"],
         "image": "https://example.com/wonder-woman.jpg"
     },
     "The super gunslinger of magma": {
-        "criteria": ["A dragon with 3 heads", "A machine-gun", "Super strenght", "In the magic reign of magma"],
+        "criteria": ["A machine-gun", "In the magic reign of magma"],
         "image": "https://example.com/flash.jpg"
-    },
-    "The super knight of magma": {
-        "criteria": ["A dragon with 3 heads", "A fire sword", "Super strenght", "In the magic reign of magma"],
+    }, #prima serie di 4
+    "The valourous melting knight": {
+        "criteria": ["A fire sword", "The ice galaxy"],
         "image": "https://example.com/batman.jpg"
     },
-    "The super archer of magma": {
-        "criteria": ["A dragon with 3 heads", "A magic bow that throws 3 arrows", "Super strenght", "In the magic reign of magma"],
+    "The super ice archer": {
+        "criteria": ["A magic bow that throws 3 arrows", "The ice galaxy"],
         "image": "https://example.com/superman.jpg"
     },
-    "The super paladin of magma ": {
-        "criteria": ["A dragon with 3 heads", "A hammer with lightnings", "Super strenght", "In the magic reign of magma"],
+    "The ice thor": {
+        "criteria": ["A hammer with lightnings", "The ice galaxy"],
         "image": "https://example.com/wonder-woman.jpg"
     },
-    "The super gunslinger of magma": {
-        "criteria": ["A dragon with 3 heads", "A machine-gun", "Super strenght", "In the magic reign of magma"],
+    "The super ice-slinger": {
+        "criteria": ["A machine-gun", "The ice galaxy"],
+        "image": "https://example.com/flash.jpg"
+    }, #fine seconda serie di 4
+    "The valorous knight of galaxies": {
+        "criteria": ["A fire sword", "In an infinity spaceship"],
+        "image": "https://example.com/batman.jpg"
+    },
+    "The mythic space archer": {
+        "criteria": ["A magic bow that throws 3 arrows", "In an infinity spaceship"],
+        "image": "https://example.com/superman.jpg"
+    },
+    "The space paladin": {
+        "criteria": ["A hammer with lightnings", "In an infinity spaceship"],
+        "image": "https://example.com/wonder-woman.jpg"
+    },
+    "The space-revolver": {
+        "criteria": ["A machine-gun", "In an infinity spaceship"],
+        "image": "https://example.com/flash.jpg"
+    }, # fine terza serie di 4
+    "The mega knight of the jungle": {
+        "criteria": ["A fire sword", "In a massive jungle"],
+        "image": "https://example.com/batman.jpg"
+    },
+    "The sneaky green archer": {
+        "criteria": ["A magic bow that throws 3 arrows", "In a massive jungle"],
+        "image": "https://example.com/superman.jpg"
+    },
+    "The paladin of thunder": {
+        "criteria": ["A hammer with lightnings", "In a massive jungle"],
+        "image": "https://example.com/wonder-woman.jpg"
+    },
+    "The tree-slinger": {
+        "criteria": ["A machine-gun", "In a massive jungle"],
         "image": "https://example.com/flash.jpg"
     }
 }
